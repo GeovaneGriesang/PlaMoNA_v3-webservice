4d849422059f3083bf168b0531ffdd97e43f7feb
TanukiDHS <Diogoschlosser@hotmail.com>
2024-04-24 21:53:59 -0300

    alteração para um metodo

--- connection.js
+++ connection.js
@@ -1,13 +1,14 @@
+const { response } = require('express');
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+export const pool = async () => {
+    return mysql.createPool ({
     host: 'databaseteste.c9yaqus04iqo.us-east-1.rds.amazonaws.com',
     user: 'admin',
     password: 'pla123mo123na123',
     database: 'geovaneg_plamona'
-}).promise();
+    }).promise();
+}
 
 
 
-
-module.exports = {pool};
