cccd8b2ad75f46ae3714d94df29ccbc7defc1c2d
TanukiDHS <Diogoschlosser@hotmail.com>
2024-04-24 21:36:43 -0300

    mudei o formato do import

--- connection.js
+++ connection.js
@@ -1,7 +1,13 @@
-import mysql from "mysql2";
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'databaseteste.c9yaqus04iqo.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'pla123mo123na123',
+    database: 'geovaneg_plamona'
+}).promise();
 
 
 
 
-
-//module.exports = pool;
+module.exports = {pool};
--- index.js
+++ index.js
@@ -1,14 +1,8 @@
  const express = require('express');
  const app = express();
  app.use(express.json());
- import mysql from "mysql2";
  const port = 3333;
- const pool = mysql.createPool({
-  host: 'databaseteste.c9yaqus04iqo.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'pla123mo123na123',
-  database: 'geovaneg_plamona'
-}).promise();
+ const pool = require('./connection');
 
 
  app.get('/', (req, res) => {
